4301e373008b39d1b6edd1df9fb4a895e1ec5522
wumian <1285837374@qq.com>
2022-12-06 22:07:31 +0800

    删除部分内容

--- deploy.bat
+++
@@ -1,4 +0,0 @@
-cd F:\CodeSpace\blogs\docs\.vuepress
-zip -q dist.zip -r dist\
-scp -r F:\CodeSpace\blogs\docs\.vuepress\dist.zip root@101.200.175.79:/root/upload
-ssh root@101.200.175.79 "rm -rf /www/wwwroot/www.brook-w.com/* && unzip /root/upload/dist.zip -d  /root/upload && mv /root/upload/dist/* /www/wwwroot/www.brook-w.com/ && rm -rf /root/upload/dist*"
